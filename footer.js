dad5381430aefa6f1d6f746e12349da02f27375e
tylerhand1 <tyha7462@colorado.edu>
2023-06-01 13:35:02 -0600

    Changed footer text from h4 to p

--- footer.js
+++ footer.js
@@ -1,3 +1,3 @@
-const footer = document.querySelector('footer h4');
+const footer = document.querySelector('footer p');
 
 footer.textContent = `${footer.textContent} ${(new Date()).getFullYear()} tylerhand1`;
